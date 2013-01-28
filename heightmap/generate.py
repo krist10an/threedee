ab31a75753b0f5c75623b55fad86d7b81a57a742
Krist10an <krist10an@hotmail.com>
2013-01-29 00:08:04 +0100

    Make script configurable from commandline with argparse

--- heightmap/generate.py
+++ heightmap/generate.py
@@ -1,13 +1,12 @@
 """
-Script for parsing elevation data and generating output for a give
-lattitude/longitude
+Script for parsing elevation data and generating output for a given lattitude/longitude
 
 Supported input:
 - STRM DEM (.hgt)
 
 Supported output:
 - Image (.jpg)
-- Model (.ply, .scad)
+- Model (.scad)
 
 Elevation data can be downloaded from:
 http://www.viewfinderpanoramas.org/dem3.html
@@ -19,6 +18,8 @@ import Image
 import numpy
 import math
 import csv
+import argparse
+import sys
 from latlon import *
 
 debug_enabled = True
@@ -35,7 +36,7 @@ def make_scad(data, outputfile, scale_x, scale_y, scale_z):
 			fd.write("%f " % (data[x,y]))
 		fd.write("\n")
 	fd.close()
-	print "Saved scad data %s" % (datafile)
+	print "Saved data %s" % (datafile)
 
 	fd = open(outputfile, "w")
 	fd.write("//\n")
@@ -46,7 +47,7 @@ def make_scad(data, outputfile, scale_x, scale_y, scale_z):
 	fd.write("      surface(file = \"%s\", center = true, convexity = 5);\n" % (os.path.basename(datafile)))
 	fd.write("};\n")
 	fd.close()
-	print "Saved scad %s" % (outputfile)
+	print "Saved SCAD %s" % (outputfile)
 
 def make_jpg(data, outfile):
 	ppix = numpy.array(data)
@@ -105,8 +106,9 @@ def generate(latlon, distance, target_size, minimum_height, remove_base_ele, inp
 	elements_to_use_y = distance / (lon_length / tilesize)
 	size_x = int(elements_to_use_x)
 	size_y = int(elements_to_use_y)
-	dbg("   Length of 1\" longitude=%f meter. Corresponds to %d array elements" % (lon_length, size_x))
-	dbg("   Length of 1\" lattitude=%f meter. Corresponds to %d array elements" % (lat_length, size_y))
+	dbg("Length of 1\" longitude=%f meter" % (lon_length))
+	dbg("Length of 1\" lattitude=%f meter" % (lat_length))
+	dbg("Using %dx%d array elements" % (size_x, size_y))
 
 	data = numpy.zeros(size_x*size_y, dtype=numpy.float64).reshape((size_x, size_y))
 	for y in xrange(size_y):
@@ -122,8 +124,9 @@ def generate(latlon, distance, target_size, minimum_height, remove_base_ele, inp
 	dbg("  Height for selected data: min=%f max=%f" % (zmin, zmax))
 
 	if remove_base_ele:
-		print "Removing base ele of %f" % (zmin)
+		print "Removing base ele of %d meters" % (zmin)
 		data = data - zmin
+		zmax = zmax - zmin
 
 	# Scale data from 0.0 to 1.0
 	data = data / data.max()
@@ -135,9 +138,9 @@ def generate(latlon, distance, target_size, minimum_height, remove_base_ele, inp
 	the_scale = distance / (target_size / 1000)
 
 	print "Model scale data:"
-	print " Model length=%.1f mm. (Real life=%d meter)" % (target_size, distance)
+	print " Model length=%.1f mm (Real life=%d meter)" % (target_size, distance)
 	print " Model height=%.1f mm (Real life=%d meter)" % (target_height, zmax)
-	print " Scale=1:%d" % (the_scale)
+	print " Model scale=1:%d" % (the_scale)
 
 	make_jpg(data*255, outfile+".jpg")
 
@@ -145,7 +148,7 @@ def generate(latlon, distance, target_size, minimum_height, remove_base_ele, inp
 	data = data * target_height
 
 	if data.min() < minimum_height:
-		print " Adding minimum height of %f" % (minimum_height)
+		print " Adding minimum height of %.1f mm" % (minimum_height)
 		data = data + minimum_height
 
 	dbg("  Height output data: min=%f max=%f" % (data.min(), data.max()))
@@ -177,21 +180,51 @@ def read_places_from_csv(placesfile):
 		except:
 			print "Invalid line", row
 			#raise
-	print "Read %d places from %s" %(len(places),  placesfile)
+	#print "Read %d places from %s" %(len(places),  placesfile)
 
 	return places
 
 
 if __name__ == "__main__":
-	target = "HUR"
+	placesfile = "places.csv"
 
-	places = read_places_from_csv("places.csv")
-	name, latlon, radius = places[target]
+	places = read_places_from_csv(placesfile)
+	targets = ["%s (%s)" % (item, places[item][0]) for item in places.keys()]
 
-	# Target sizes (in mm)
-	target_size = 100
-	min_height = 0.6
-	remove_base_ele = True
+	import argparse
+	parser = argparse.ArgumentParser(description="Generate heightmap from elevation data")
+	parser.add_argument('target',
+		nargs=1,
+		help='The target to generate. Targets available from '+placesfile+' : ' + ' '.join(targets))
+	parser.add_argument('-d', '--debug',dest='debug', action='store_const',
+		const=True, default=False, help='Enable debug output')
+	parser.add_argument('-i', '--input', dest='input', default="input/",
+		help='Folder to read elevation data')
+	parser.add_argument('-o', '--output', dest='output', default="output/heightmap",
+		help='Folder and filename for output')
+	parser.add_argument('-s', '--size', dest='size', type=float, default=80,
+		help='Output size (in millimeter). Default: 80')
+	parser.add_argument('-m', '--minheight', dest='minheight', type=float, default=0.6,
+		help='Minimum height for elevation data (in millimeter). Default: 0.6')
+	parser.add_argument('-b', '--base', dest='base', action="store_const", const=True, default=False,
+		help='Remove base elevation')
+
+	args = parser.parse_args()
+
+	debug_enabled = args.debug
+	target = args.target[0]
+	infolder = args.input
+	outfolder = args.output
+	target_size = args.size
+	min_height = args.minheight
+	remove_base_ele = args.base
+
+	try:
+		name, latlon, radius = places[target]
+	except KeyError:
+		parser.print_help()
+		print "\nError: '%s' is not a valid key" % (target)
+		sys.exit(1)
 
 	print "== Generating %s ==" % (name)
-	generate(latlon, radius, target_size, min_height, remove_base_ele, "input/", "output/heightmap")
+	generate(latlon, radius, target_size, min_height, remove_base_ele, infolder, outfolder)
